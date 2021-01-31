bb9b55c12663ff6427617d85880e52407c884a37
anastasiiatorbunova <54706156+anastasiiatorbunova@users.noreply.github.com>
2021-01-30 22:01:46 -0700

    JS

---
+++ landing/assets/js/script.js
@@ -0,0 +1,13 @@
+//Smooth scroll to register section
+const smoothLinks = document.querySelectorAll('a[href^="#"]');
+for (let smoothLink of smoothLinks) {
+    smoothLink.addEventListener('click', function (e) {
+        e.preventDefault();
+        const id = smoothLink.getAttribute('href');
+
+        document.querySelector(id).scrollIntoView({
+            behavior: 'smooth',
+            block: 'start'
+        });
+    });
+};
