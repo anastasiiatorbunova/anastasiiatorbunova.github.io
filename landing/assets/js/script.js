acf16f54c8c049269d0bf4222ad20714b4abe573
anastasiiatorbunova <54706156+anastasiiatorbunova@users.noreply.github.com>
2021-02-01 09:10:05 -0700

    Third commit

--- landing/assets/js/script.js
+++ landing/assets/js/script.js
@@ -1,13 +1,33 @@
-//Smooth scroll to register section
-const smoothLinks = document.querySelectorAll('a[href^="#"]');
-for (let smoothLink of smoothLinks) {
-    smoothLink.addEventListener('click', function (e) {
+jQuery(document).ready(function($) {
+    
+    //Smooth scroll to register section
+    $('a[href^="#"]').on('click', function(e){
         e.preventDefault();
-        const id = smoothLink.getAttribute('href');
+        var id = $(this).attr('href');
 
-        document.querySelector(id).scrollIntoView({
-            behavior: 'smooth',
-            block: 'start'
-        });
+        $('body,html').animate({
+            scrollTop: $(id).offset().top
+        }, 1500, 'linear');
     });
-};
+
+    $('.ag-desc p').each(function(){
+        
+        var arr = $(this).html().split('<!--more-->');
+        
+        if( arr.length > 1 ){
+           arr.splice(3, 0, '<div class="read-more-container"><a class="more" id="readMoreBTN">read more</a></div>');
+            arr[0] = '<p>'+arr[0]+'</p>';
+            arr[1] = '<p class="hidden">'+arr[1]+'</p>';
+            $(this).replaceWith( arr.join('') );
+        } 
+        
+    });
+    
+    $('#readMoreBTN').on('click', function(){
+        let n = 0;
+        $(this).parents('.ag-desc').find('.hidden').slideToggle( 'slow');
+        $(this).text( $(this).text() == 'read more' ? 'read less' : 'read more' );
+    });
+    
+});
+
